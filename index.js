66ed39b038c0a6c5c0c536513a3634231083a217
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2026-06-25 17:44:13 -0500

    package.json, and initial index

---
+++ index.js
@@ -0,0 +1,37 @@
+async function submitResult({
+  accessToken,
+  lineItem,
+  userId,
+  type,
+  score,
+  maxScore,
+  url
+}) {
+  const body = {
+    userId,
+    activityProgress: "Completed",
+    gradingProgress: "FullyGraded"
+  };
+
+  if (type === "grade") {
+    body.scoreGiven = score;
+    body.scoreMaximum = maxScore;
+  }
+
+  if (type === "link") {
+    body.comment = `View submission: ${url}`;
+  }
+
+  return fetch(lineItem + "/scores", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/vnd.ims.lis.v1.score+json"
+    },
+    body: JSON.stringify(body)
+  });
+}
+
+module.exports = {
+  submitResult
+};
